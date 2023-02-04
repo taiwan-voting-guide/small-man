f6139aac266699bebf24952f62f942df2382e21e
gnituy18 <you@hsuyuting.com>
2023-02-04 12:31:31 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,65 @@
+import { Configuration, OpenAIApi } from "openai";
+import bolt from "@slack/bolt";
+
+const configuration = new Configuration({
+  organization: process.env.OPENAI_ORG,
+  apiKey: process.env.OPENAI_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const app = new bolt.App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
+
+app.event("app_mention", async ({ event, client, context }) => {
+  try {
+    const text = event.text.replace(/<@.*>/, "").trim();
+    if (!text.includes("old:") || !text.includes("new:")) {
+      await sendMsg(
+        client,
+        context.botToken,
+        event.channel,
+        "請提供舊法案與新法案 'old:' 'new:'"
+      );
+      return;
+    }
+
+    const res = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `old=舊法條 new=新法條 ${text} 100個中文字內解釋新舊條文之間的差異與改變意義:`,
+      max_tokens: 256,
+      temperature: 0.5,
+      top_p: 1,
+      n: 1,
+      stream: false,
+      logprobs: null,
+      stop: "Old",
+    });
+
+    await sendMsg(
+      client,
+      context.botToken,
+      event.channel,
+      res.data.choices[0].text
+    );
+  } catch (error) {
+    await sendMsg(client, context.botToken, event.channel, "錯誤");
+  }
+});
+
+async function sendMsg(client, token, channel, text) {
+  try {
+    await client.chat.postMessage({
+      token,
+      channel,
+      text,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+(async () => {
+  await app.start(process.env.PORT || 3000);
+})();
